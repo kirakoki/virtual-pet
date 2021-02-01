c3509301769c46a279432871ff5bdb64c79db789
Kemal Akyigit <kemal.akyigit@hotmail.com>
2021-02-01 17:12:03 +0000

    example-commit

--- tests/vpet.test.js
+++ tests/vpet.test.js
@@ -1,95 +1,117 @@
-const Pet = require('../vpet.js'); 
+const Pet = require("../vpet.js");
 
 let pet;
 beforeEach(() => {
-    pet = new Pet("Fido");
+  pet = new Pet("Fido");
 });
 
-describe("virtual pet constructor", ()=>{
-    
-    it("should check if the pet constructor is defined",() => {
-        expect(pet).toBeDefined();
-    });
-    it('returns an object', () => {
-        expect(pet).toBeInstanceOf(Object);
-    });
-    it('sets the name property', () => {
-        expect(pet.name).toEqual("Fido");        
-    });
-    it('has a initial age of 0', () => {
-        expect(pet.age).toBe(0); 
-    });   
-    it('has a initial hunger of 0', () => {
-        expect(pet.hunger).toBe(0); 
-    });
-    it('has a initial fitness of 10', () => {
-        expect(pet.fitness).toBe(10); 
-    });
-
+describe("virtual pet constructor", () => {
+  it("should check if the pet constructor is defined", () => {
+    expect(pet).toBeDefined();
+  });
+  it("returns an object", () => {
+    expect(pet).toBeInstanceOf(Object);
+  });
+  it("sets the name property", () => {
+    expect(pet.name).toEqual("Fido");
+  });
+  it("has a initial age of 0", () => {
+    expect(pet.age).toBe(0);
+  });
+  it("has a initial hunger of 0", () => {
+    expect(pet.hunger).toBe(0);
+  });
+  it("has a initial fitness of 10", () => {
+    expect(pet.fitness).toBe(10);
+  });
 });
 
-describe("Grow up feature",()=>{
-   beforeEach(()=>{
-       pet.growUp();
-    });
+describe("Grow up feature", () => {
+  beforeEach(() => {
+    pet.growUp();
+  });
 
-    it("increments the age by 1", ()=>{ 
-        expect(pet.age).toBe(1);
-    });
-    it("increments the hunger by 5", ()=>{ 
-        expect(pet.hunger).toBe(5);
-    });
-    it("decreases the fitness by 3", ()=>{ 
-        expect(pet.fitness).toBe(7);
-    });
-
+  it("increments the age by 1", () => {
+    expect(pet.age).toBe(1);
+  });
+  it("increments the hunger by 5", () => {
+    expect(pet.hunger).toBe(5);
+  });
+  it("decreases the fitness by 3", () => {
+    expect(pet.fitness).toBe(7);
+  });
 });
 
-describe("Keeping fit feature",()=>{
-    it("increments the fitness by 4", ()=>{
-        pet.fitness = 0;
-        pet.walk();
-        expect(pet.fitness).toBe(4);
-    });
-    it("increments the fitness to a maximum value of 10",() =>{
-        pet.fitness = 7;
-        pet.walk();
-        expect(pet.fitness).toBe(10);
-    });
-
+describe("Keeping fit feature", () => {
+  it("increments the fitness by 4", () => {
+    pet.fitness = 0;
+    pet.walk();
+    expect(pet.fitness).toBe(4);
+  });
+  it("increments the fitness to a maximum value of 10", () => {
+    pet.fitness = 7;
+    pet.walk();
+    expect(pet.fitness).toBe(10);
+  });
 });
 
-describe ("Keeping fed feature", ()=>{
-    it("decreases hunger level by 3", ()=>{
-        pet.hunger = 5;
-        pet.feed();
-        expect(pet.hunger).toBe(2);
-    });
-    it("hunger level doesnt go below 0", ()=>{
-        pet.hunger = 2;
-        pet.feed();
-        expect(pet.hunger).toBe(0);
-    });
+describe("Keeping fed feature", () => {
+  it("decreases hunger level by 3", () => {
+    pet.hunger = 5;
+    pet.feed();
+    expect(pet.hunger).toBe(2);
+  });
+  it("hunger level doesnt go below 0", () => {
+    pet.hunger = 2;
+    pet.feed();
+    expect(pet.hunger).toBe(0);
+  });
 });
-describe ("Check up", ()=>{
-    it("checks pet's fitness",()=>{
-        pet.fitness =2;
-        pet.hunger= 3;
-        expect(pet.checkUp()).toBe("I need a walk");
-    });
-    it("checks pet's hunger",()=>{
-        pet.fitness =4;
-        pet.hunger = 6        
-        expect(pet.checkUp()).toBe("I am hungry");
-    });
-    it("checks both pet's fitness & hunger",()=>{
-        pet.fitness = 2;
-        pet.hunger = 7;    
-        expect(pet.checkUp()).toBe('I am hungry AND I need a walk');
-    });
-    it("checks if nothing else is needed",()=>{
-        pet.fitness =4;
-        pet.hunger= 4;  
-        expect(pet.checkUp()).toBe("I feel great!");
-    });
-});     
+describe("Check up", () => {
+  it("checks both pet's fitness & hunger", () => {
+    pet.fitness = 2;
+    pet.hunger = 7;
+    expect(pet.checkUp()).toBe("I am hungry AND I need a walk");
+  });
+  it("checks pet's fitness", () => {
+    pet.fitness = 2;
+    pet.hunger = 3;
+    expect(pet.checkUp()).toBe("I need a walk");
+  });
+  it("checks pet's hunger", () => {
+    pet.fitness = 4;
+    pet.hunger = 6;
+    expect(pet.checkUp()).toBe("I am hungry");
+  });
+
+  it("checks if nothing else is needed", () => {
+    pet.fitness = 4;
+    pet.hunger = 4;
+    expect(pet.checkUp()).toBe("I feel great!");
+  });
+});
+
+describe("Is alive?", () => {
+  it("checks if the pet is alive when its fitness is 0", () => {
+    pet.fitness = 0;
+    expect(pet.isAlive).toBe(false);
+  });
+  it("checks if the pet is alive when its hunger is 10 or above", () => {
+    pet.fitness = 3;
+    pet.hunger = 10;
+    expect(pet.isAlive).toBe(false);
+    pet.hunger = 11;
+    expect(pet.isAlive).toBe(false);
+  });
+  it("checks if the pet is alive when it is 30 years old or above", () => {
+    pet.hunger = 8;
+    pet.age = 30;
+    expect(pet.isAlive).toBe(false);
+    pet.hunger = 31;
+    expect(pet.isAlive).toBe(false);
+  });
+  it("checks if the pet is alive",()=>{
+    pet. age = 10;
+    expect(pet.isAlive).toBe(true);
+  });
+});
--- vpet.js
+++ vpet.js
@@ -1,36 +1,55 @@
 function Pet(name) {
-    this.name=name;
-    this.age =0;
-    this.hunger=0;
-    this.fitness=10;
+  this.name = name;
+  this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
 }
 
 Pet.prototype = {
-    growUp (){
-        this.age += 1;
-        this.hunger +=5;
-        this.fitness -=3;
-    },
-    walk (){
-        const MAXIMUM_FITNESS = 10;
-        this.fitness +=4;
-        if (this.fitness >= MAXIMUM_FITNESS){
-            this.fitness = MAXIMUM_FITNESS;
-        }
-    },
-    feed (){
-        this.hunger -=3;
-        if (this.hunger <0){
-            this.hunger = 0;
-        }
-    },
-    checkUp (){
-        if(this.fitness <= 3 && this.hunger >=5){'I am hungry AND I need a walk'}
-        if(this.fitness <= 3) {return 'I need a walk'}
-        if(this.hunger >=5) {return 'I am hungry'}
-        else return 'I feel great!'
+  get isAlive() {
+    // if (this.fitness === 0) {
+    //   return false;
+    // } 
+    // if(this.hunger >= 10){
+    //     return false;
+    // }
+    // if(this.age >= 30){
+    //     return false;
+    // }
+    // else {
+    //     return true;
+    // }
+    return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+  },
+  growUp() {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+  },
+  walk() {
+    const MAXIMUM_FITNESS = 10;
+    this.fitness += 4;
+    if (this.fitness >= MAXIMUM_FITNESS) {
+      this.fitness = MAXIMUM_FITNESS;
     }
-}
-
+  },
+  feed() {
+    this.hunger -= 3;
+    if (this.hunger < 0) {
+      this.hunger = 0;
+    }
+  },
+  checkUp() {
+    if (this.fitness < 3 && this.hunger >= 5) {
+      return "I am hungry AND I need a walk";
+    }
+    if (this.fitness <= 3) {
+      return "I need a walk";
+    }
+    if (this.hunger >= 5) {
+      return "I am hungry";
+    } else return "I feel great!";
+  }
+};
 
 module.exports = Pet;
